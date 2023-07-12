35f115372a1930e06d5d646addf784fb518dc4c9
motikev <kmotties@gmail.com>
2023-07-12 16:05:58 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,3 +1,70 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, '').trim();
+  }
+
+  static titleize(str) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      if (!smallWords.includes(words[i]) || i === 0) {
+        words[i] = this.capitalize(words[i]);
+      }
+    }
+
+    return words.join(' ');
+  }
 }
+
+// test cases here
+
+describe( "Formatter", () => {
+  describe( "capitalize", () => {
+    it( "is a static method", () => {
+      expect( () => Formatter.capitalize( "crocodile" ) ).to.not.throw()
+    } )
+
+    it( "uppercases the first letter in a String", () => {
+      expect( Formatter.capitalize( "a" ) ).to.equal( "A" )
+      expect( Formatter.capitalize( "alligator" ) ).to.equal( "Alligator" )
+      expect( Formatter.capitalize( "BURSTING BALLOON" ) ).to.equal( "BURSTING BALLOON" )
+    } )
+  } )
+
+  describe( "sanitize", () => {
+    it( "is a static method", () => {
+      expect( () => Formatter.sanitize( "C catching colds" ) ).to.not.throw()
+    } )
+
+    it( "removes non-alphanumeric characters except for dash, single quote and space", () => {
+      expect( Formatter.sanitize( "c" ) ).to.equal( "c" )
+      expect( Formatter.sanitize( "!" ) ).to.equal( "" )
+      expect( Formatter.sanitize( "ca$@#tching cold" ) ).to.equal( "catching cold" )
+      expect( Formatter.sanitize( "Doin' Dishes" ) ).to.equal( "Doin' Dishes" )
+      expect( Formatter.sanitize( "Entert*ain(i{ng-Elephan^ts" ) ).to.equal( "Entertaining-Elephants" )
+    } )
+  } )
+
+  describe( "titleize", () => {
+    it( "is a static method", () => {
+      expect( () => Formatter.titleize( "F forever foolish" ) ).to.not.throw()
+    } )
+
+    it( "capitalizes all words in a sentence except 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'", () => {
+      expect( Formatter.titleize( "getting giggles" ) ).to.equal( "Getting Giggles" )
+      expect( Formatter.titleize( "where the wild things are" ) ).to.equal( "Where the Wild Things Are" )
+      expect( Formatter.titleize( "chicken soup with rice and a few other songs" ) ).to.equal( "Chicken Soup with Rice and a Few Other Songs" )
+      expect( Formatter.titleize( "Maurice a an but of and for at by from end" ) ).to.equal( "Maurice a an but of and for at by from End" )
+    } )
+
+    it( "always capitalizes the first word", () => {
+      expect( Formatter.titleize( "a tale of two cities" ) ).to.equal( "A Tale of Two Cities" )
+      expect( Formatter.titleize( "in the night kitchen" ) ).to.equal( "In the Night Kitchen" )
+    } )
+  } )
+} )
